9e540b25c1f251a37581b65cf1f1748225f17c37
Muneeb Ur Rehman <realmuneeburrehman@gmail.com>
2023-12-31 13:55:33 +0500

    pinia

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,9 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
+  modules: [
+    '@pinia/nuxt',
+  ],
   devServer: {
     host: "127.0.0.1"
   }
